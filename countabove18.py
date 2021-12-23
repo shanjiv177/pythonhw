d8a356e486f993aaddb1c9cf8e28747054a87aec
shanjiv177 <shanjiv177@gmail.com>
2021-12-23 22:56:32 +0530

    Counting above 18

---
+++ countabove18.py
@@ -0,0 +1,27 @@
+
+boolean = True
+nameList = []
+ageList = []
+
+while boolean == True:
+    name = input("What is the person's name? :")
+    age = int(input("What is the person's age (in numbers)?: "))
+
+
+
+    nameList.append(name)
+    ageList.append(age)
+
+    yorn = input("Do you want to add an another person?: (yes or no)")
+    if yorn.lower() == "yes" or yorn.lower() == "y":
+        boolean = True
+    elif yorn.lower() == "no" or yorn.lower() == "n":
+        boolean = False
+        print("Calculating...")
+
+number = 0
+
+for i in range (0, len(nameList)):
+    if ageList[i] > 18:
+        number +=1
+print("Number of people above 18 years of age is: " , number)
