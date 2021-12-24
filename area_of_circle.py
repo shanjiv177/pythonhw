9008f95b9c5ad7ab48426d8c14b4d07c9b91c2b6
Shanjiv A <65117343+shanjiv177@users.noreply.github.com>
2021-12-24 08:00:32 +0530

    Added perimetee

--- area_of_circle.py
+++ area_of_circle.py
@@ -1,3 +1,6 @@
+import pi from math
 rad = int(input("What is the radius of the circle? :"))
-area = (22/7)*rad*rad
-print(area)
+
+print(f"Area of the circle is{pi*(r*r)}:")
+perimeter = 2*pi*rad
+print("Perimeter of the Circle is:", perimeter)
